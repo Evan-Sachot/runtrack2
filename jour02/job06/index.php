35ce75f396ec6ebb01a924beae7f9b0479c21d29
evan-sachot <evan.sachot@laplateforme.io>
2025-07-10 15:38:25 +0200

    job07

--- jour02/job06/index.php
+++ jour02/job06/index.php
@@ -3,19 +3,11 @@
 
 $height = 10;
 
- 
-
 $image = imagecreate($width, $height);
-
- 
-
 $color = imagecolorallocate($image, 255, 0, 0);
-
- 
 
 imagerectangle($image,0,0,$width,$height,$color);
  
-
 header("Content-Type: image/png");
 
 imagepng($image);
