0721fb9f2dbd7dbfc17d7263e9ea25c0d68edda1
evan-sachot <evan.sachot@laplateforme.io>
2025-07-10 13:27:27 +0200

    Add files via upload

---
+++ runtrack2/jour01/job01/index.php
@@ -0,0 +1,15 @@
+ <?php
+$str = "LaPlateforme";
+$str2 = "Vive";
+$str3 = "!";
+echo "$str2";
+echo "$str" ;
+echo "$str3 ";
+echo "<br>";
+$val = "6";
+echo "$val";
+echo "<br>";
+echo $val+4;
+$myBool = true;
+echo "<br>"."$myBool";
+?>
