ca97834a86d15830efef5282662dbe15d0c74777
evan-sachot <evan.sachot@laplateforme.io>
2025-07-22 11:25:20 +0200

    job 07 php fini

--- jour07/job07/index.php
+++ jour07/job07/index.php
@@ -20,17 +20,48 @@
   <input type="submit" value="Submit">
 </form>
 <?php
+$decal = 1;
 $str = $_GET["str"];
 $select= $_GET["fonction"];
 $wc = str_word_count($str,1);
-if($select==="gras"){
 
-// preg_match_all('([A-Z][^\s]*)', $str, $matches);
-// foreach($matches[0] as $w){
-// $str = str_replace($w,"<b>$w<b>",$str);
+function gras($wc){ 
+    for($i=0;$i<count($wc);$i++){
+        if(ctype_upper($wc[$i][0])){
+        echo "<b>$wc[$i]</b> ";
+        }
+        else{
+        echo "$wc[$i] ";
 }
-echo "$str"; 
+}
+}
+function cesar($str,$decal){
+$alpha=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+    for($i=0 ; $i < strlen($str);$i++){
+    $arc= $i;
+    $keywithdecal = array_search($str[$i],$alpha)+$decal;
+    $resultat= $alpha[$keywithdecal];
+    print_r($resultat);
+    }
+}
+function plateforme($wc){
+    
+    for($i=0;$i<count($wc);$i++){
+    $sub= substr_count($wc[$i][-1],"e");
+    $sub1=substr_count($wc[$i][-2],"m");
+        if($sub===1||$sub1===1){
+        echo "$wc[$i]"."_ ";
+        }
+        else{
+        echo "$wc[$i] ";
+        }
+    }
+}
+if($select==="gras"){gras($wc);}
 
+if($select==="cesar"){cesar($str,$decal);}
+
+if($select==="plateforme"){plateforme($wc);}
 
 ?>
 </body>
