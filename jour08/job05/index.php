2e1e9c88da468b811f0987078588b796fef60602
evan-sachot <evan.sachot@laplateforme.io>
2025-07-29 11:07:10 +0200

    ok

--- jour08/job05/index.php
+++ jour08/job05/index.php
@@ -17,19 +17,19 @@
     <form method="get">
 <table>
 <tr>
-    <td><button type="submit" value="-"></button></td>
-    <td><button type="submit" value="-"></button></td>
-    <td><button type="submit" value="-"></button></td>
+    <td><button type="submit" name="button1" value="-"></button></td>
+    <td><button type="submit" name="button2" value="-"></button></td>
+    <td><button type="submit" name="button3" value="-"></button></td>
 </tr>
 <tr>
-    <td><button type="submit" value="-"></button></td>
-    <td><button type="submit" value="-"></button></td>
-    <td><button type="submit" value="-"></button></td>
+    <td><button type="submit" name="button4" value="-"></button></td>
+    <td><button type="submit" name="button5" value="-"></button></td>
+    <td><button type="submit" name="button6" value="-"></button></td>
 </tr>
 <tr>
-    <td><button type="submit" value="-"></button></td>
-    <td><button type="submit" value="-"></button></td>
-    <td><button type="submit" value="-"></button></td>
+    <td><button type="submit" name="button7" value="-"></button></td>
+    <td><button type="submit" name="button8" value="-"></button></td>
+    <td><button type="submit" name="button9" value="-"></button></td>
 </tr>
 </table>
 </form>
