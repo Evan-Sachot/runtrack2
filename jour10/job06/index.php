1423e682aef59f51a7d4df21d2901e203c16f3d7
evan-sachot <evan.sachot@laplateforme.io>
2025-08-05 09:59:01 +0200

    ok

--- jour10/job06/index.php
+++ jour10/job06/index.php
@@ -2,7 +2,7 @@
 $servname = "localhost"; $user = "root"; $pass = ""; $dbname = "jour09";
 
 $conn = new mysqli($servname, $user, $pass, $dbname);
-$sql = "SELECT count(*) as total FROM etudiants";
+$sql = "SELECT count(*) as nb_etudiants FROM etudiants";
 $result = $conn->query($sql);
 var_dump($result);
 ?>
@@ -22,7 +22,7 @@ var_dump($result);
         if ($result->num_rows > 0) {
             while($row = $result->fetch_assoc()) {
                 echo "<tr>";
-                echo "<td>" . $row["total"] . "</td>";
+                echo "<td>" . $row["nb_etudiants"] . "</td>";
                 echo "</tr>";
             }
         } else {
